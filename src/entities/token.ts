1073e21343ae23d9e5474a73320f88416056642c
Shaun <37434846+ShaunLWM@users.noreply.github.com>
2021-11-06 23:08:37 +0800

    Fix WBNB address on Testnet (#9)

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -76,7 +76,7 @@ export const WETH = {
   ),
   [ChainId.TESTNET]: new Token(
     ChainId.TESTNET,
-    '0xaE8E19eFB41e7b96815649A6a60785e1fbA84C1e',
+    '0x094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F',
     18,
     'WBNB',
     'Wrapped BNB',
